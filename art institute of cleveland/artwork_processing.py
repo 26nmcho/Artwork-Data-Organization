368b3a6b2e3fc919ff5721fb39c44f85ebf48131
Noah Cho <noahcho@frc4322.com>
2026-09-10 14:59:57 -0700

    first attempt at data validation method

--- art institute of cleveland/artwork_processing.py
+++ art institute of cleveland/artwork_processing.py
@@ -1,14 +1,25 @@
 import json
 import requests
 
+# trackers
+no_id = 0
+no_title = 0
+no_desc = 0
+no_early_date = 0
+no_late_date = 0 
+no_creator = 0
+no_technique = 0
+no_image = 0
 
-def print_openaccess_results():
+
+
+def data_collection(skip, limit):
     url = "https://openaccess-api.clevelandart.org/api/artworks"
     params = {
             'has_image': 1,
-            'limit': 250,
+            'limit': limit,
             'type': 'Painting',
-            'skip' : 0,
+            'skip' : skip,
             'fields' : 'id,title,description,creation_date_earliest,creation_date_latest,creators,technique,type,images,url,share_license_status'
         }
 
@@ -26,24 +37,61 @@ def print_openaccess_results():
         artwork_date_latest = artwork.get('creation_date_latest')
         artwork_creators = artwork.get('creators')
         artwork_technique = artwork.get('technique')
-        artwork_type = artwork.get('type')
         try:
             artwork_image = artwork['images']['web']['url']
         except KeyError:
-            artwork_image = "None"
-        print(f"ID: {artwork_id}")
-        print(f"Title: {artwork_title}")
-        print(f"Description: {artwork_desc}")
-        print(f"Earliest Creation Date: {artwork_date_earliest}")
-        print(f"Latest Creatoin Date: {artwork_date_latest}")
-        creator_index = 1
+            artwork_image = None
+        creator_list = []
         for creator in artwork_creators:
             name = creator.get('description')
-            print(f"Creator No. {creator_index}: {name}")
-        print(f"Technique: {artwork_technique}")
-        print(f"Type: {artwork_type}")
-        print(f"Image Details: {artwork_image}")
+            creator_list.add(name)
+        artwork = {
+            'id' : artwork_id,
+            'title' : artwork_title,
+            'description' : artwork_desc,
+            'creation date early' : artwork_date_earliest,
+            'creation date late' : artwork_date_latest,
+            'creators' : artwork_creators,
+            'technique' : artwork_technique,
+            'image' : artwork_image
+        }
+
+def data_validation(artwork):
+    return_boolean = True
+    if artwork['id'] == None:
+        return_boolean = False
+        global no_id
+        no_id += 1
+    elif artwork['title'] == None:
+        return_boolean = False
+        global no_title
+        no_title += 1
+    elif artwork['description'] == None:
+        return_boolean = False
+        global no_desc
+        no_desc += 1
+    elif artwork['creation date early'] == None:
+        return_boolean = False
+        global no_early_date
+        no_early_date += 1
+    elif artwork['creation date late'] == None:
+        return_boolean = False
+        global no_late_date
+        no_late_date += 1
+    elif artwork['creators'] == None:
+        return_boolean = False
+        global no_creator
+        no_creator += 1
+    elif artwork['technique'] == None:
+        return_boolean = False
+        global no_technique
+        no_technique += 1
+    elif artwork['image'] == None:
+        return_boolean = False
+        global no_image
+        no_image += 1
+    return return_boolean
 
 
 if __name__ == '__main__':
-    print_openaccess_results()
+    data_collection(0, 250)
